23a5d08b77f6f69a04171c31482f7d98dccce38e
Vishnu-2k5 <vishnuswarooph@gmail.com>
2025-02-19 15:51:11 +0530

    mysql and python connection added

---
+++ Day3/db1.py
@@ -0,0 +1,16 @@
+import pymysql
+def connect_db():
+    try:
+        connection=pymysql.Connect(host='localhost',port=3306,user='root',password='root',database='vishnu_db',charset='utf8')
+        print('DB Connected')
+    except:
+        print('DB Connection failed')
+    return connection
+def disconnect_db(connection):
+    try:
+        connection.close()
+        print('DB Disconnecetd')
+    except:
+        print("DB Disconnection failed")
+connection=connect_db()
+disconnect_db(connection)    
